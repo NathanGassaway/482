37e58f1e6da04650344bc2a689dec4ba29fbebdd
nathan <nathangway@gmail.com>
2015-12-22 23:01:36 -0500

    new files and video testing

--- home.js
+++ home.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
+
 	
 	document.getElementById("video").controls = false;
 
 	//if user is still at top of page
 	setTimeout(function(){
 		if($(window).scrollTop() === 0) {
-			$(".header-container").show();
+			$(".content-container").show();
 			// $("html, body").animate({
 			// 	scrollTop: $(".header-container").offset().top
 			// }, 1000);
