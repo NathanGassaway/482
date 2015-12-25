f223e0a8b5c343ed104853f744abfd0268087d90
nathan <nathangway@gmail.com>
2015-12-24 22:01:14 -0500

    adding landing

--- home.js
+++ home.js
@@ -1,15 +1,21 @@
 $(document).ready(function(){
 
-	
-	document.getElementById("video").controls = false;
+	var openLanding = document.getElementById("landing-video");
 
-	//if user is still at top of page
-	setTimeout(function(){
-		if($(window).scrollTop() === 0) {
-			$(".content-container").show();
-			// $("html, body").animate({
-			// 	scrollTop: $(".header-container").offset().top
-			// }, 1000);
-	   	}
-   	}, 2000)
+	// document.getElementById("video").controls = false;
+
+	// //if user is still at top of page
+	// setTimeout(function(){
+	// 	if($(window).scrollTop() === 0) {
+	// 		$(".content-container").show();
+	// 		// $("html, body").animate({
+	// 		// 	scrollTop: $(".header-container").offset().top
+	// 		// }, 1000);
+	//    	}
+ //   	}, 2000)
+
+	openLanding.onclick = function(){
+    window.location.href = "http://www.nathangassaway.com/home";
+  }
+
 });
