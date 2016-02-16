b7242f42f95076ac523fda807974e41dffb90392
nathan <nathangway@gmail.com>
2016-02-16 18:52:54 -0500

    fixed back button

--- home.js
+++ home.js
@@ -5,22 +5,8 @@ $(document).ready(function(){
 	var arrowContainer = document.getElementById("arrowContainer"); //function in play.js
 
 	$(".arrow-button").click(function(){
-		window.location.href = "http://www.nathangassaway.com/home";
+		window.location.href = "http://www.nathangassaway.com/";
 	});
-
-	$.ajax({
-	url:"https://www.kimonolabs.com/api/429gz3gu?apikey=onTT1v1xy7KG7WEYMgtZXlRHeHCnWEpn",
-	crossDomain: true,
-	dataType: "jsonp",
-	success: function (response) {
-	//Do something with the response
-	var okay = response.results.collection1[0].property1
-	console.log(okay)
-	},
-	error: function (xhr, status) {
-	console.log(error);
-	}
-	}),
 
 	openLanding.onclick = function(){
 	window.location.href = "http://www.nathangassaway.com/home";
